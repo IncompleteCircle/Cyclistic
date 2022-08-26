fa93b3faa6c341305688909a631eabbd5cbd40ca
John <43623335+IncompleteCircle@users.noreply.github.com>
2022-08-26 11:50:06 +0200

    Update fd_00_main.R

--- main/fd_00_main.R
+++ main/fd_00_main.R
@@ -15,10 +15,10 @@ rm(package_there)
 
 #1: Data Phase: Process [Pull Data]
 #File Designation Numbers: 01
-source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/data/process/fd_01_data_process_integrate_2020.R")
+source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/tools/process/fd_01_data_process_integrate_2020.R")
 
 #2: Data Phase: Clean
 #File Designation Numbers: 02-04
-source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/data/clean/fd_02_data_clean_timedate_2020.R")
-source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/data/clean/fd_03_data_clean_NAs_analysis_2020.R")
-source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/data/clean/fd_04_data_clean_merge_2020.R")
+source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/tools/clean/fd_02_data_clean_timedate_2020.R")
+source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/tools/clean/fd_03_data_clean_NAs_analysis_2020.R")
+source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/tools/clean/fd_04_data_clean_merge_2020.R")
