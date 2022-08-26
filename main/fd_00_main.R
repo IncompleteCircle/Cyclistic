3e153ca101ae78e4253178dbf1da95b061106f38
John <43623335+IncompleteCircle@users.noreply.github.com>
2022-08-26 20:29:28 +0200

    Update fd_00_main.R

--- main/fd_00_main.R
+++ main/fd_00_main.R
@@ -22,3 +22,7 @@ source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/to
 source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/tools/clean/fd_02_data_clean_timedate_2020.R")
 source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/tools/clean/fd_03_data_clean_NAs_analysis_2020.R")
 source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/tools/clean/fd_04_data_clean_merge_2020.R")
+
+#3: Data Phase: Exploratory Data Analysis [EDA]
+#File Designation Number: 05
+source_url("https://raw.githubusercontent.com/IncompleteCircle/Cyclistic/main/tools/eda/fd_05_eda_divvy_bike_2020.R")
